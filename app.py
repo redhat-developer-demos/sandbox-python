9606f1db903ab91cb62e9efe8f74e9351eb65ac2
Don Schenck <don.schenck@gmail.com>
2022-04-20 13:53:34 -0400

    removed debug mode

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8080, debug=True)
+    app.run(host="localhost", port=8080, debug=False)
