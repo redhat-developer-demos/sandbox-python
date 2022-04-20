7d93ca85cd6890ad1ff832f2b564f4d7ee697d70
Don Schenck <don.schenck@gmail.com>
2022-04-20 14:13:14 -0400

    Host port set to 0.0.0.0

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=8080, debug=False)
+    app.run(host="0.0.0.0", port=8080, debug=False)
